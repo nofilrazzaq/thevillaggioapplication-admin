6d242ec0ed6745cf29f6a3bf4af3eb9357767fc3
Nofil Razzaq <nofilrazzaq11@gmail.com>
2022-04-08 16:52:45 +0500

    api_url updated

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://api.thevillaggioapplication.com/public/api"',
-  MAIN_DOMAIN: '"https://thevillaggioapplication.com"'
+  API_URL: '"https://villaggio-api.tsahousing.com/public/api"',
+  MAIN_DOMAIN: '"https://villaggio-application.tsahousing.com"'
 }
